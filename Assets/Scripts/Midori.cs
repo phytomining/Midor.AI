a3ba097c35864eb111012a9fbd134fe1348a88a6
Awevy <Awevyyy@gmail.com>
2024-03-24 23:28:02 +0000

    Lol 11:28 late update
    updated camera (true camera freedom)

---
+++ Assets/Scripts/Midori.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float maxHealth = 100;
+    public float health = 100;
+    public float speed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
