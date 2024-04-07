03d1d4382c3a438c50501e1bbc004bed6feab826
Midori Gurin <midori.gurinishi@outlook.com>
2024-04-07 15:19:48 +0300

    Added CPU text!
    CPU-meter is an INTEGRAL element to the gameplay... after all, Midori here is an Android! So when you utilize lets say 100% of your CPU your game will in the future behave differently!!
    
    meow~~~

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -16,23 +16,22 @@ public class UI : MonoBehaviour
     public RawImage healthBarFillBg;
 
     public float health = 100f, maxHealth = 100f;
-
+    public float currentcpu = 0f, lerptocpu = 0f;
     public Text healthText, cpuText;
     public Color yellow = Color.yellow, red = Color.red, green;
     public float duration = 0.2f;
 
     private Color initialColor;
     private float timer = 0f, imgtimer = 0f;
-    public int zone = 0;
-    IEnumerator ChangeColorOverTime(Text text, Color targetColor)
+    public int zone = 0, cpuzone = 0;
+    IEnumerator ChangeColorOverTime(Text text, Color targetColor, Color initcol)
     {
         while (timer < duration)
         {
             float t = timer / duration;
-            Color lerpedColor = Color.Lerp(initialColor, targetColor, t);
+            Color lerpedColor = Color.Lerp(initcol, targetColor, t);
             text.color = lerpedColor;
             timer += Time.deltaTime;
-            Debug.Log("Changing color to: " + lerpedColor);
             yield return null; 
         }
         text.color = targetColor;
@@ -47,7 +46,6 @@ public class UI : MonoBehaviour
             Color lerpedColor = Color.Lerp(initialColor, targetColor, t);
             img.color = lerpedColor;
             imgtimer += Time.deltaTime;
-            Debug.Log("Changing color to: (IMG)" + lerpedColor);
             yield return null; 
         }
         //img.color = targetColor;
@@ -86,21 +84,21 @@ public class UI : MonoBehaviour
         rectTransform.localScale = newScale;
     }
 
-    void Start()
-    {
-        float realWidth = healthBarFill.rectTransform.sizeDelta.x;
-        midori = player.GetComponent<Player>().midori;
-        Debug.Log(midori.name);
-        Debug.Log(midori.GetComponent<Midori>());
-        health = midori.GetComponent<Midori>().health;
-        maxHealth = midori.GetComponent<Midori>().maxHealth;
-        realWidth = healthBarFill.rectTransform.sizeDelta.x;
-        UpdateHealth(health);
-    }
-
     public void UpdateHealthText(float currentHealth, float maxHealth)
     {
-        healthText.text = "Health: " + ((float)Math.Round(currentHealth, 1)).ToString() + " / " + ((float)Math.Round(maxHealth, 1)).ToString();
+        healthText.text = "Health: " + ((float)Math.Round(currentHealth, 1)).ToString() + " / " + ((float)Math.Round(maxHealth, 1)).ToString("F1");
+    }
+
+    public float cpuLerpToCalculate()
+    {
+        float val = 0f;
+        val += player.gameObject.GetComponent<Rigidbody>().velocity.magnitude * 10;
+        if(modeActive) val += 50f;
+        return Mathf.Min(100f, val);
+    }
+    public void UpdateCPUText(float currentCPU)
+    {
+        cpuText.text = "CPU: " + ((float)Math.Round(currentCPU, 1)).ToString("F1") + "%";
     }
 
     public void UpdateHealth(float currentHealth)
@@ -110,27 +108,51 @@ public class UI : MonoBehaviour
         ChangeScale(new Vector3(6f * healthPercentage, 1.05f,1f));
         CropImage(healthPercentage);
 
-        Debug.Log(zone + " " + healthPercentage);
-
         if(healthPercentage >= 0.6f && zone != 2){
-            Debug.Log("Test " + healthPercentage);
             initialColor = healthText.color;
-            StartCoroutine(ChangeColorOverTime(healthText, green));
+            StartCoroutine(ChangeColorOverTime(healthText, green, initialColor));
             StartCoroutine(ChangeColorOverTimeImage(healthBarFill, green));
             zone = 2;
         }else if(healthPercentage >= 0.2f && healthPercentage < 0.6f && zone != 1){
             initialColor = healthText.color;
-            StartCoroutine(ChangeColorOverTime(healthText, yellow));
+            StartCoroutine(ChangeColorOverTime(healthText, yellow, initialColor));
             StartCoroutine(ChangeColorOverTimeImage(healthBarFill, yellow));
             zone = 1;
         }else if(zone != 0 && healthPercentage < 0.2){
             initialColor = healthText.color;
-            StartCoroutine(ChangeColorOverTime(healthText, red));
+            StartCoroutine(ChangeColorOverTime(healthText, red, initialColor));
             StartCoroutine(ChangeColorOverTimeImage(healthBarFill, red));
             zone = 0;
         }
     }
 
+    public void UpdateCPU(){
+        Debug.Log(cpuzone + " " + currentcpu);
+        if(currentcpu >= 66.6f && cpuzone != 2){
+            initialColor = cpuText.color;
+            StartCoroutine(ChangeColorOverTime(cpuText, red, initialColor));
+            cpuzone = 2;
+        }else if(currentcpu >= 33.3f && currentcpu < 66.6f && cpuzone != 1){
+            initialColor = cpuText.color;
+            StartCoroutine(ChangeColorOverTime(cpuText, yellow, initialColor));
+            cpuzone = 1;
+            Debug.Log("TESTEESTESTSTETSST");
+        }else if(cpuzone != 0 && currentcpu < 33.3f){
+            initialColor = cpuText.color;
+            StartCoroutine(ChangeColorOverTime(cpuText, Color.green, initialColor));
+            cpuzone = 0;
+        }
+    }
+
+    void Start()
+    {
+        float realWidth = healthBarFill.rectTransform.sizeDelta.x;
+        midori = player.GetComponent<Player>().midori;
+        health = midori.GetComponent<Midori>().health;
+        maxHealth = midori.GetComponent<Midori>().maxHealth;
+        realWidth = healthBarFill.rectTransform.sizeDelta.x;
+        UpdateHealth(health);
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.LeftControl) && !modeActive)
@@ -155,5 +177,13 @@ public class UI : MonoBehaviour
             changeAlphaText(healthText,(float)Math.Round(Mathf.Lerp(healthBarFill.color.a, 0f, lerpSpeed * Time.deltaTime),4));
         }   
         if(Input.GetKey(KeyCode.T)) midori.GetComponent<Midori>().damage(0.1f);
+    }
+    
+    void FixedUpdate()
+    {
+        lerptocpu = cpuLerpToCalculate();
+        currentcpu = Mathf.Lerp(currentcpu, lerptocpu, 1.3f * Time.deltaTime);
+        UpdateCPUText(currentcpu);
+        UpdateCPU();
     }
 }
